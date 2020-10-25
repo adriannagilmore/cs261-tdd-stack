458b538d923e3aa2dd6f55a87f4942805e9f1e81
Adrianna Gilmore <a_inman33@yahoo.com>
2020-10-24 21:58:17 -0700

    11 tests pass

--- stack.py
+++ stack.py
@@ -4,4 +4,26 @@
 
 class Stack:
 
-    pass
+    def __init__(self):
+        self.myStack = []
+
+    def is_empty(self):
+        if len(self.myStack) == 0:
+            return True
+        else:
+            return False
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError
+        else: 
+            return self.myStack.pop()
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError
+        else:
+            return self.myStack[-1]
+
+    def push(self, item):
+        self.myStack.append(item)
